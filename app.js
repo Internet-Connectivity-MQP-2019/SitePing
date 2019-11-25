86409b30427eab46744dc646ae43f20b41954bed
samgoldman <sgoldman216@gmail.com>
2019-11-25 18:37:39 -0500

    Remove HTTPS redirect

--- app.js
+++ app.js
@@ -8,15 +8,6 @@ fs.createReadStream('GeoIP2-City.zip').pipe(unzip.Extract({ path: '.' }));
 
 const app = express();
 
-// Force HTTPS
-app.use((req, res, next) => {
-  // The 'x-forwarded-proto' check is for Heroku
-  if (!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV !== "development") {
-    return res.redirect('https://' + req.get('host') + req.url);
-  }
-  next();
-});
-
 app.use(bodyParser.json());
 app.use(compression());
 app.use(express.static('dist'));
