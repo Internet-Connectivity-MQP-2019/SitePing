df7cf5a0b404ebfb0f85dffe048091c373db7d8e
samgoldman <sgoldman216@gmail.com>
2020-01-17 14:31:25 -0500

    Create generic state_map.js and update dns_display to use it.

--- src/javascript/dns_display.js
+++ src/javascript/dns_display.js
@@ -2,135 +2,67 @@ import "@babel/polyfill"
 
 import * as d3Base from 'd3';
 import {group} from 'd3-array';
-import * as topojson from 'topojson';
+import * as state_map from './state_map';
 
 const d3 = Object.assign(d3Base, {group});
 
 const width = 500;
 const height = 300;
 
-const mapSvg1 = d3.select("#map_div_1")
-	.append("svg")
-	.attr("width", width)
-	.attr("height", height);
+let map1 = null;
+let map2 = null;
+let map3 = null;
 
-const mapSvg2 = d3.select("#map_div_2")
-	.append("svg")
-	.attr("width", width)
-	.attr("height", height);
-
-const mapSvg3 = d3.select("#map_div_3")
-	.append("svg")
-	.attr("width", width)
-	.attr("height", height);
-
-const projection = d3.geoAlbersUsa()
-	.translate([width / 2, height / 2])
-	.scale([650]);
-
-const path = d3.geoPath()
-	.projection(projection);
-
-let states = null;
 const dns_data = {};
-d3.json("us-named.topojson").then(us => {
-	states = topojson.feature(us, us.objects.states);
-
-	mapSvg1.selectAll("path")
-		.data(states.features)
-		.enter()
-		.append("path")
-		.attr("d", path)
-		.style("fill", function (d) {
-			return '#7f7f7f';
-		});
-	mapSvg2.selectAll("path")
-		.data(states.features)
-		.enter()
-		.append("path")
-		.attr("d", path)
-		.style("fill", function (d) {
-			return '#7f7f7f';
-		});
-	mapSvg3.selectAll("path")
-		.data(states.features)
-		.enter()
-		.append("path")
-		.attr("d", path)
-		.style("fill", function (d) {
-			return '#7f7f7f';
-		});
-
-	d3.csv('results_aggregate_median_aggregate_mean.csv').then(data => {
-		data.forEach(d => {
-			dns_data[d['recursive_state']] = d;
-		});
-
-		updateMap();
+d3.csv('results_aggregate_median_aggregate_mean.csv').then(data => {
+	data.forEach(d => {
+		dns_data[d['recursive_state']] = d;
 	});
+
+	// Once data is loaded, display the maps.
+	createMaps();
+	updateMaps();
 });
 
-let scaledGradient = d3.scaleSequential(d3.interpolateOrRd)
+// Gradient for maps one and three
+// TODO: make gradient automatically update
+const scaledGradient = d3.scaleSequential(d3.interpolateOrRd)
 	.domain([30, 100]);
-let scaledGradient2 = d3.scaleSequential(d3.interpolateRdBu)
+// Gradient for map two
+const scaledGradient2 = d3.scaleSequential(d3.interpolateRdBu)
 	.domain([-20, 20]);
 
-const updateMap = function() {
-		const map1_column = document.querySelector("#map_select_1").value;
-		const map3_column = document.querySelector("#map_select_3").value;
-		mapSvg1.selectAll("path").remove();
-		mapSvg1.selectAll("path")
-			.data(states.features)
-			.enter()
-			.append("path")
-			.attr("d", path)
-			.style("fill", function (d) {
-				if (d.properties.code in dns_data) {
-					const stateAverage = parseFloat(dns_data[d.properties.code][map1_column]);
-					if (stateAverage === undefined || isNaN(stateAverage) || stateAverage === null) {
-						return '#7f7f7f'
-					}
-					return scaledGradient(stateAverage);
-				}
-
-				return '#7f7f7f';
-			});
-	mapSvg3.selectAll("path").remove();
-	mapSvg3.selectAll("path")
-		.data(states.features)
-		.enter()
-		.append("path")
-		.attr("d", path)
-		.style("fill", function (d) {
-			if (d.properties.code in dns_data) {
-				const stateAverage = parseFloat(dns_data[d.properties.code][map3_column]);
-				if (stateAverage === undefined || isNaN(stateAverage) || stateAverage === null) {
-					return '#7f7f7f'
-				}
-				return scaledGradient(stateAverage);
-			}
-
-			return '#7f7f7f';
-		});
-	mapSvg2.selectAll("path").remove();
-	mapSvg2.selectAll("path")
-		.data(states.features)
-		.enter()
-		.append("path")
-		.attr("d", path)
-		.style("fill", function (d) {
-			if (d.properties.code in dns_data) {
-				const stateAverage = parseFloat(dns_data[d.properties.code][map3_column]) - parseFloat(dns_data[d.properties.code][map1_column]);
-				if (stateAverage === undefined || isNaN(stateAverage) || stateAverage === null) {
-					return '#7f7f7f'
-				}
-				return scaledGradient2(stateAverage);
-			}
-
-			return '#7f7f7f';
-		});
+const createMaps = () => {
+	map1 = state_map.default.createStateMap('#map_div_1', width, height, 650, '#7f7f7f');
+	map2 = state_map.default.createStateMap('#map_div_2', width, height, 650, '#7f7f7f');
+	map3 = state_map.default.createStateMap('#map_div_3', width, height, 650, '#7f7f7f');
 };
 
-document.querySelector("#map_select_1").onchange = updateMap;
-document.querySelector("#map_select_3").onchange = updateMap;
+const updateMaps = function() {
+	const map1_column = document.querySelector("#map_select_1").value;
+	const map3_column = document.querySelector("#map_select_3").value;
+
+	const map1_state_dictionary = {};
+	for (let [key, value] of Object.entries(dns_data)) {
+		map1_state_dictionary[key] = parseFloat(value[map1_column]);
+	}
+
+	// Map two is the difference between the columns for one and three
+	const map2_state_dictionary = {};
+	for (let [key, value] of Object.entries(dns_data)) {
+		map2_state_dictionary[key] = parseFloat(value[map3_column]) - parseFloat(value[map1_column]);
+	}
+
+	const map3_state_dictionary = {};
+	for (let [key, value] of Object.entries(dns_data)) {
+		map3_state_dictionary[key] = parseFloat(value[map3_column]);
+	}
+
+	state_map.default.updateStateMap('#map_div_1', map1, scaledGradient, map1_state_dictionary);
+	state_map.default.updateStateMap('#map_div_2', map2, scaledGradient2, map2_state_dictionary);
+	state_map.default.updateStateMap('#map_div_3', map3, scaledGradient, map3_state_dictionary);
+};
+
+document.querySelector("#map_select_1").onchange = updateMaps;
+document.querySelector("#map_select_3").onchange = updateMaps;
 
---
+++ src/javascript/state_map.js
@@ -0,0 +1,98 @@
+import * as d3Base from 'd3';
+import {group} from 'd3-array';
+import * as topojson from 'topojson';
+
+const d3 = Object.assign(d3Base, {group});
+
+let state_data = null;
+d3.json("us-named.topojson").then(us => {
+	state_data = topojson.feature(us, us.objects.states);
+});
+
+
+// Initialize a state map. The div_id must include a '#'. The map will be drawn with defaultColor for each state.
+// Use updateStateMap to update the colors.
+const createStateMap = (div_id, width, height, scale, defaultColor) => {
+	const div = document.querySelector(div_id);
+
+	// Initialize the projection and path based on the user's parameters
+	const projection = d3.geoAlbersUsa()
+		.translate([width / 2, height / 2])
+		.scale([scale]);
+
+	const path = d3.geoPath()
+		.projection(projection);
+
+	// Store information about this map in the div
+	div.dataset.map_width = width;
+	div.dataset.map_height = height;
+	div.dataset.map_scale = scale;
+	div.dataset.default_color = defaultColor;
+
+	// Create the SVG
+	const svg = d3.select(div_id)
+		.append("svg")
+		.attr("width", width)
+		.attr("height", height);
+
+	// Allow initialization of the state data
+	while (state_data === null) {}
+
+	// Draw the map with the default color
+	svg.selectAll("path")
+		.data(state_data.features)
+		.enter()
+		.append("path")
+		.attr("d", path)
+		.style("fill", div.dataset.default_color);
+
+	return svg;
+};
+
+// Again, div_id must be a valid div id with a '#'. The svg is the value returned by createStateMap.
+// Color gradient is used to get colors.
+// State dictionary is a map from state code ('AZ', 'CO', ...) OR state name to a value that is passed into the gradient
+const updateStateMap = (div_id, svg, colorGradient, stateDictionary) => {
+	const div = document.querySelector(div_id);
+
+	// Retrieve stored constants
+	const width = parseInt(div.dataset.map_width);
+	const height = parseInt(div.dataset.map_height);
+	const scale = parseInt(div.dataset.map_scale);
+	const default_color = div.dataset.default_color;
+
+	// Recreate the projection and path used for this map
+	const projection = d3.geoAlbersUsa()
+		.translate([width / 2, height / 2])
+		.scale([scale]);
+
+	const path = d3.geoPath()
+		.projection(projection);
+
+	// Remove elements
+	svg.selectAll("path").remove();
+
+	// Redraw elements
+	svg.selectAll("path")
+		.data(state_data.features)
+		.enter()
+		.append("path")
+		.attr("d", path)
+		.style("fill", function (d) {
+			if (d.properties.code in stateDictionary || d.properties.name in stateDictionary) {
+				const stateAverage = d.properties.code in stateDictionary ?
+					parseFloat(stateDictionary[d.properties.code]) :
+					parseFloat(stateDictionary[d.properties.name]);
+
+				if (stateAverage !== undefined && ! isNaN(stateAverage) && stateAverage !== null) {
+					return colorGradient(stateAverage);
+				}
+			}
+
+			// Couldn't get a value for some reason, fall back to default
+			return default_color;
+		});
+};
+
+
+export default {createStateMap, updateStateMap}
